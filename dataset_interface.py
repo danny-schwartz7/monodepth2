bc9f78b0550e7bb90c2c6f0ff86a047c4da52c86
Parker Killion <pkillion99@gmail.com>
2022-03-06 14:39:42 -0800

    reshaped

--- dataset_interface.py
+++ dataset_interface.py
@@ -232,13 +232,13 @@ class MyDataset(torch.utils.data.Dataset):
         print(f"has shape: {depth_gtL.size()} and {depth_gtR.size()}")
         if depth_gtL.size() != (375, 1242):
             depth_gtL = resizeT(depth_gtL.unsqueeze(0))
+            depth_gtL.squeeze(0)
             print("new L depth Size:", depth_gtL.size())
 
-        if depth_gtR.size() != (375, 1242):
-            depth_gtR = F.interpolate(depth_gtR, (375, 1242))
-            print("new RSize:", depth_gtR.size())
-            raise
 
-        #data_tuple : Data_Tuple = Data_Tuple(imgL, imgR, depth_gtL, depth_gtR, focalLength, baseline)
+        if depth_gtR.size() != (375, 1242):
+            depth_gtR = resizeT(depth_gtR.unsqueeze(0))
+            depth_gtR.squeeze(0)
+            print("new R depth Size:", depth_gtR.size())
 
         return (imgL, imgR, depth_gtL, depth_gtR, focalLength, baseline)
