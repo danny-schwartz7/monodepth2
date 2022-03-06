60a85e5b20b73ed16c2e634e28533ae235b3bb33
Danny Schwartz <deschwa2@illinois.edu>
2022-03-05 16:07:22 -0800

    Add option to use upsampling instead of convTranspose in EncDecNet

--- unsupervised/EncDecNet.py
+++ unsupervised/EncDecNet.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from typing import Tuple, List
 from collections import deque
 
-from unsupervised.Blocks import ConvElu, UpConvElu, Reshaper, ResConv
+from unsupervised.Blocks import ConvElu, UpConvElu, Reshaper, ResConv, UpsampleAndSmooth
 
 MAX_DISP_FRAC = 0.3  # from the paper
 
@@ -12,27 +12,32 @@ MAX_DISP_FRAC = 0.3  # from the paper
 class EncDecNet(nn.Module):
     # take the place of the bowtie-looking thing in the diagrams of https://arxiv.org/pdf/1609.03677.pdf
 
-    def __init__(self):
+    def __init__(self, mode: str = "upsample"):
         super(EncDecNet, self).__init__()
 
-        self._init_model()
+        self._init_model(mode)
 
         # start with 3 x 375 x 1242
         # first, get down to a reasonable resolution
 
+    def assert_mode(self, mode: str):
+        assert mode in ["upsample", "upconv"]
+
     def forward(self, x: torch.Tensor) -> List[Tuple[torch.Tensor, torch.Tensor]]:
         """
-        TODO: in the unsupervised monocular depth paper, they output disp at 4 different scales. Should we do this?
 
         :param x: an input tensor of shape (N, C, H, W) where N is batch size,
             C is # channels (e.g., RGB = 3), H and W are height and width
             IMPORTANT: RGB input is expected to be floating point between 0 and 1
-        :return: a tuple of two disparity maps, left-to-right disp
+        :return: a list of tuples of two disparity maps, left-to-right disp
             and right-to-left disp (dimensions determined by KITTI).
             These maps will have elements in [-1, 1] and can be transformed
             into a more conventional view of disparity by subtracting
             torch.linspace(-1, 1, width) from each column and then
-            multiplying by width to get a number of pixels
+            multiplying by width to get a number of pixels.
+
+            Each tuple in the list corresponds to a different output scale. The list is ordered with
+            ascending resolution (more granular predictions are toward the end of the list).
         """
 
         # use a deque to implement skip connections
@@ -49,8 +54,9 @@ class EncDecNet(nn.Module):
 
         for i, module in enumerate(self.skip_up_blocks):
             skip = skip_tensor_deque.pop()
-            if i > 2 and i < (len(self.skip_up_blocks) - 1):
-                x = module.upconv(x + skip)
+            if i > 1 and i < (len(self.skip_up_blocks) - 1):
+                # assume 'module' is of type 'UpConvElu' or 'UpsampleAndSmooth'
+                x = module.up(x + skip)
                 disp_part = x[:, :2, :, :]
                 disp_part = MAX_DISP_FRAC * F.sigmoid(disp_part)
                 left_to_right_disp = disp_part[:, 0, :, :]
@@ -72,7 +78,7 @@ class EncDecNet(nn.Module):
         # The largest scale is the last element of this list
         return disp_pairs_at_scales
 
-    def _init_model(self):
+    def _init_model(self, mode: str = "upsample"):
         self.skip_down_blocks = nn.ModuleList([
             ConvElu(3, 4, 5, 2),
             ConvElu(4, 4, 5, 2),
@@ -95,19 +101,32 @@ class EncDecNet(nn.Module):
             Reshaper((1, 8, 16)),
             UpConvElu(1, 4, 1, 1, 0, 0)
         ])
-        self.skip_up_blocks = nn.ModuleList([
-            UpConvElu(4, 32, (3, 7), 1, 0, 0),
-            UpConvElu(32, 16, (3, 7), 2, 0, 0),
-            UpConvElu(16, 8, (3, 7), (2, 3), 0, 1),
-            UpConvElu(8, 4, 5, 2, 0, (0, 1)),
-            UpConvElu(4, 4, 5, 2, 0, (1, 0)),
 
-            # non-smoothed output:
-            # UpConvElu(4, 2, 5, 2, 0, (0, 1), activation=False)
+        if mode == "upconv":
+            self.skip_up_blocks = nn.ModuleList([
+                UpConvElu(4, 32, (3, 7), 1, 0, 0),
+                UpConvElu(32, 16, (3, 7), 2, 0, 0),
+                UpConvElu(16, 8, (3, 7), (2, 3), 0, 1),
+                UpConvElu(8, 4, 5, 2, 0, (0, 1)),
+                UpConvElu(4, 4, 5, 2, 0, (1, 0)),
 
-            # smoothed output:
-            UpConvElu(4, 4, 5, 2, 0, (0, 1))
-        ])
+                # non-smoothed output:
+                # UpConvElu(4, 2, 5, 2, 0, (0, 1), activation=False)
+
+                # smoothed output:
+                UpConvElu(4, 4, 5, 2, 0, (0, 1))
+            ])
+        elif mode == "upsample":
+            self.skip_up_blocks = nn.ModuleList([
+                UpsampleAndSmooth(in_channels=4, out_channels=32, out_height=10, out_width=22, smooth_kernel_size=3),
+                UpsampleAndSmooth(in_channels=32, out_channels=16, out_height=21, out_width=49, smooth_kernel_size=3),
+                UpsampleAndSmooth(in_channels=16, out_channels=8, out_height=44, out_width=152),
+                UpsampleAndSmooth(in_channels=8, out_channels=4, out_height=91, out_width=308),
+                UpsampleAndSmooth(in_channels=4, out_channels=4, out_height=186, out_width=619),
+                UpsampleAndSmooth(in_channels=4, out_channels=4, out_height=375, out_width=1242)
+            ])
+        else:
+            raise ValueError(f"Invalid mode {mode}")
         self.smoothing_blocks = nn.ModuleList([
             ResConv(4, 7, 2),
             nn.Conv2d(4, 2, 7, padding="same")
