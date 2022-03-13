c4e44ef70b322a72098ec21cf19f7dff46153793
Danny Schwartz <deschwa2@illinois.edu>
2022-03-13 13:32:39 -0700

    Update test.py script

--- project/test.py
+++ project/test.py
@@ -1,8 +1,8 @@
 import argparse
 import torch
-from torch.utils.data import DataLoader
+from matplotlib import pyplot as plt
 
-from project.ModelingUtils import test, visualize_disparity_maps
+from project.ModelingUtils import test, data_tuple_to_plt_image
 from project.train import make_dir_if_not_exists
 from dataset_interface import MyDataset, get_dataloader
 
@@ -13,8 +13,12 @@ def get_args():
     parser.add_argument('--model-save-dir', type=str, required=True)
     parser.add_argument('--batch-size', type=int, default=20)
 
+    parser.add_argument('--skip-metrics', action="store_true",
+                        help="specify this to skip computation of depth performance metrics.")
     parser.add_argument('--visualize-dir', type=str,
                         help="location of directory to save visualized results to")
+    parser.add_argument('--num-viz-examples-per-split', type=int, default=5,
+                        help="number of examples per dataset split to visualize")
 
     return parser.parse_args()
 
@@ -24,19 +28,22 @@ def main():
 
     model = torch.load(f"{args.model_save_dir}/best.pt")
     test_loader = get_dataloader("test", batch_size=args.batch_size, shuffle=False)
-    test(test_loader, model)
 
-    # TODO: Parker's new data-tuple arrangement invalidated the following code, we should fix it at some point
+    if args.skip_metrics is None:
+        test(test_loader, model)
+
     if args.visualize_dir is not None:
-        # make_dir_if_not_exists(args.visualize_dir)
-        #
-        # # TODO: should eventually be test loader, not train loader
-        # viz_loader = DataLoader(dataset=MyDataset("train"), batch_size=args.batch_size)
-        #
-        # save_path = f"{args.visualize_dir}/viz.png"
-        # visualize_disparity_maps(viz_loader, model, save_path)
+        make_dir_if_not_exists(args.visualize_dir)
+        for split in ["train", "eval", "test"]:
+            dataset = MyDataset(split)
+            for i in range(args.num_viz_examples_per_split):
+                viz_tup = dataset[i]
 
-        print("Visualization code is not implemented to work with new Data_Tuple type, do this yourself if you want to use it.")
+                fig = data_tuple_to_plt_image(viz_tup, model)
+                save_path = f"{args.visualize_dir}/{split}_{i}.png"
+                plt.savefig(save_path)
+                plt.close(fig)
+            del dataset
 
 
 if __name__ == "__main__":
